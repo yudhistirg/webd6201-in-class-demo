958d7b4343ead9dfb30faea9b3c2378ab63758a6
Yudhistir <gnawaliyudhistir@gmail.com>
2023-01-28 22:49:16 -0500

    *

--- scripts/EventHandler.js
+++ scripts/EventHandler.js
@@ -4,18 +4,18 @@ var displayEvent = document.getElementById("displayEvent");
 var userDisplay = document.getElementById("userDisplay");
 var passDisplay = document.getElementById("passDisplay");
 
-welcomeLabel.addEventListener("dblclick", function () {
-    console.log("Event: dblclick");
-    displayEvent.textContent = "Event: dblclick";
-});
-welcomeLabel.addEventListener("mouseover", function () {
-    console.log("Event: mouseover");
-    displayEvent.textContent = "Event: mouseover";
-});
-welcomeLabel.addEventListener("mouseout", function () {
-    console.log("Event: mouseout");
-    displayEvent.textContent = "Event: mouseout";
-});
+// welcomeLabel.addEventListener("dblclick", function () {
+//     console.log("Event: dblclick");
+//     displayEvent.textContent = "Event: dblclick";
+// });
+// welcomeLabel.addEventListener("mouseover", function () {
+//     console.log("Event: mouseover");
+//     displayEvent.textContent = "Event: mouseover";
+// });
+// welcomeLabel.addEventListener("mouseout", function () {
+//     console.log("Event: mouseout");
+//     displayEvent.textContent = "Event: mouseout";
+// });
 
 function validateForm() {
     let x = document.forms["myForm"]["username"].value;
