4ed2065f67f1c0fe26457976fc7b63d3b329a5ec
Yudhistir <gnawaliyudhistir@gmail.com>
2023-02-05 20:03:51 -0500

    Completed ICE4

--- ICE4/scripts/main.js
+++ ICE4/scripts/main.js
@@ -1,7 +1,6 @@
-
 (function () {
 
-    function DisplayButton() {
+    function DisplayHome() {
         let randomButton = document.getElementById("RandomButton")
         randomButton.addEventListener("click", function() {
             location.href = 'projects.html'
@@ -30,21 +29,72 @@
 
         // add after(append)
         mainContent.appendChild(mainParagraph) 
-        
-        // add before (before)
-        // mainContent.before(mainParagraph) 
+    }
 
-        // delete element
-        // document.getElementById("RandomButton").remove()
+    function DisplayProjects() {
+        console.log("Projects Page")
+    }
 
-        // documentBody.innerHTML = `
-        //     <div class="container">
-        //         <h1 class="display-1">Hello WEBD6201</h1>
-        //         <p class="mt-5 lead">and... what do you this of this method?</p>
-        //     </div>
-        // 
-        let sohaib = new Contact ("Sohaib Mohiuddin", "6471234567","sohaib.mohiuddin@durhamcollege.ca")
-        console.log(sohaib.toString())`
+    function DisplayContacts() {
+        console.log("Contact Us Page")
+
+        let submitButton = document.getElementById("submitButton")
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox")
+
+        // localStorage Example
+        // localStorage.setItem("Random Variable", "random variable for testing and demonstration")
+        // console.log(localStorage.getItem("Random Variable"))
+        // localStorage.removeItem("Random Variable")
+
+        submitButton.addEventListener("click", function() {
+            // event.preventDefault()
+            if (subscribeCheckbox.checked) {
+                // If user subscribes, store the contact in localStorage
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value)
+                if (contact.serialize()) {
+                    let key = contact.Name.substring(0, 1) + Date.now()
+                    localStorage.setItem(key, contact.serialize())
+                }
+            }
+        })
+    }
+
+    function DisplayContactList() {
+        if (localStorage.length > 0) {
+            let contactList = document.getElementById("contactList") // Our contact list in the table of the contact-list page
+
+            let data = "" // Add data to this variable. Append deserialized data from localStorage to data
+            let keys = Object.keys(localStorage) // Return a String Array of keys
+
+            let index = 1 // Count number of keys
+
+            // for every key in the keys collection
+            for (const key of keys) {
+                let contactData = localStorage.getItem(key) // Get localStorage data value related to the key
+                let contact = new Contact()
+                
+                contact.deserialize(contactData)
+
+                // Inject repeatable row into the contactList
+                data += `<tr>
+                    <th scope="row" class="text-center">${ index }</th>
+                    <td class="text-center">${ contact.Name }</td>
+                    <td class="text-center">${ contact.ContactNumber }</td>
+                    <td class="text-center">${ contact.EmailAddress }</td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                </tr>
+                `
+
+                index++
+            }
+
+            contactList.innerHTML = data
+        }
+    }
+
+    function DisplayReferences() {
+        console.log("References Page")
     }
     
     function Start() {
@@ -52,7 +102,19 @@
 
         switch (document.title) {
             case "Home - WEBD6201 Demo":
-                DisplayButton()
+                DisplayHome()
+                break
+            case "Projects - WEBD6201 Demo":
+                DisplayProjects()
+                break
+            case "Contact Us - WEBD6201 Demo":
+                DisplayContacts()
+                break
+            case "Contact List - WEBD6201 Demo":
+                DisplayContactList()
+                break
+            case "References - WEBD6201 Demo":
+                DisplayReferences()
                 break
         }
     }
